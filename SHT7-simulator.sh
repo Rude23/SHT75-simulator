33f45e947a01dcb16befba7aea6c21c7eb141f62
Fulvio Fantacone <fulvio.fantacone@gmail.com>
2024-11-18 20:30:08 +0000

    INIT COMMIT

---
+++ SHT7-simulator.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+function interrupt(){
+    echo "CTRL+C detected. Cleaning up and exiting..."
+    exit $?
+}
+
+function display_usage() {
+    echo "$NAME is a simple script to simulate SHT75 data stream sampled through an XXXXXXXXX programmable microcontroller" #TODO: inserire nome scheda
+    echo "developed as a tool for studying data acquisition required in lab classes for \"università degli studi di Napoli Federico II\""
+    echo "LM17 master degree during A.A. 2024-2025"
+    echo ""
+    echo "Usage: $NAME {[-p --port] [-d --debug] [-t --period] | -h --help}"
+    echo "Arguments:"
+    echo "  -p --port     : (virtual) usb port to write bytes to"
+    echo "  -t --period  : Specify the period (in seconds) for a full data (end-word, T, HR) sampling cycle"
+}
+
+trap interrupt SIGINT
+
+NAME="SHT7-simulator"
+
+VALID_ARGS=$(getopt -o hp:t: --long help,period:,port: -- "$@")
+if [[ $? -ne 0 ]]; then
+    exit 1;
+fi
+
+eval set -- "$VALID_ARGS"
+while [ : ]; do
+  case "$1" in
+    -p | --port)
+      if [ -n "$2" ]; then
+        PORT=$2
+        shift 2
+      else
+        display_usage
+        exit 1
+      fi
+      ;;
+    -t | --period)
+        if [[ $2 =~ ^[0-9]+(\.[0-9]+)?$ ]]; then
+          PERIOD_3=$2
+          shift 2
+        else
+          display_usage
+          exit 1
+        fi
+        ;;
+    -h | --help)
+        "echo H"
+        display_usage
+        exit 0
+        ;;
+    *)
+        break
+        ;;
+  esac
+done
+
+if [[ -z "$PERIOD_3" ]]; then
+    "echo T"
+    display_usage
+    exit 1;
+fi
+
+if [[ -z "$PORT" ]]; then
+    "echo P"
+    display_usage
+    exit 1;
+fi
+
+while :
+do
+  sleep "$PERIOD_3";
+  echo -n -e '\xAA\xAA' > "$PORT";
+  sleep "$PERIOD_3";
+  hexdump -vn 1 -e '1/1 "%02x"' /dev/urandom > "$PORT";
+  sleep "$PERIOD_3";
+  hexdump -vn 1 -e '1/1 "%02x"' /dev/urandom > "$PORT";
+done
